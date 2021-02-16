99f9da5a1408ba5f5341bea269d3bcb77a11f6c5
Vedant Arya <v.arya2004.va@gmail.com>
2021-02-17 00:50:48 +0530

    added config.ini, colors, filtered least_busy output for non-simulator and smaller than 4 qubit systems, starting qrng

--- api_request.py
+++ api_request.py
@@ -3,8 +3,11 @@ from qiskit.visualization import plot_histogram,plot_state_city
 import argparse
 from argparse import RawTextHelpFormatter
 import sys
-import colorama
-
+from colorama import Fore,Style,Back,init
+from configparser import ConfigParser
+from qiskit.backends.jobstatus import JOB_FINAL_STATES
+import time
+init(autoreset=False)
 '''
 Available systems for this account:
 
@@ -20,7 +23,10 @@ Available systems for this account:
 Least busy system: ibmq_armonk
 ibmq_armonk
 '''
-token='024257bbd2ae5845824e3650cd45561fca6cdff0284dc34870113b886144a3ded1c691a8d263d37fb3e5b282a5a5bcdf4573296e632221992acf75cfabb1f65c'
+config=ConfigParser()
+config.read('config.ini')
+token=config.get('auth','token')
+#token='024257bbd2ae5845824e3650cd45561fca6cdff0284dc34870113b886144a3ded1c691a8d263d37fb3e5b282a5a5bcdf4573296e632221992acf75cfabb1f65c'
 #get token or something here, declare a glob var
 class notyetdecided:
     def __init__(self,simul=True,listsys=False,leastbusy=False,tok=None):
@@ -38,7 +44,7 @@ class notyetdecided:
             try:
                 _providers=IBMQ.load_account()
             except:
-                print('Error occured during \'IBMQ.load_account()\'')
+                print('[{0}!{1}]Error occured during \'IBMQ.load_account()\''.format(Fore.RED,Fore.RESET))
                 sys.exit(0)
             if self.listsys:
                 providers1=[i for i in dir(_providers.backends) if 'ibmq' in i]
@@ -46,22 +52,27 @@ class notyetdecided:
                 print('\nAvailable systems for this account: \n')
     #            print('\n|{0}||-----||'.format(str(-)*int(_f),))
                 for _ in providers1:
-                    print('|{:<20}|'.format(_),sep='|',end='\n')            #_providers.backends.______.status_msg'.format(_) could've been used for status of systems
+                    k=_providers.get_backend(_)
+                    print('|{}{:<20}{}| {}{:<2}{} |'.format(Fore.MAGENTA,_,Fore.RESET,Fore.WHITE,k.configuration().n_qubits,Fore.RESET),sep='|',end='\n')            #_providers.backends.______.status_msg'.format(_) could've been used for status of systems
                 backend0=providers.ibmq.least_busy(_providers.backends(simulator=False)).name()
-                print('\nLeast busy system: {}'.format(backend0))
+                print('\nLeast busy system: {0}{1}{2}'.format(Fore.GREEN,backend0,Fore.RESET))
                 sys.exit(0)
             try:
                 if self.leastbusy:
-                    backend0=providers.ibmq.least_busy(_providers.backends(simulator=False)).name()
-                    print('\nLeast busy system: {}\n'.format(backend0))
+                    #for _ in _providers.backends(simulator=False).__iter__(): #remove smaller than 4 qubit systems for least_busy()
+                    #    if _providers.get_backend(_).configuration().n_qubits>=4:
+                    _filteredproviders=[_ for _ in _providers.backends(simulator=False).__iter__() if _providers.get_backend(str(_)).configuration().n_qubits>=4]
+                    #backend0=providers.ibmq.least_busy(_providers.backends(simulator=False))
+                    backend0=providers.ibmq.least_busy(_filteredproviders)
+                    print('\nLeast busy system: {0}{1}{2}\n'.format(Fore.GREEN,backend0.name(),Fore.RESET))
                     backend=_providers.get_backend(backend0)
                 if needsystem:
                     backend=_providers.get_backend(system)
             except QiskitBackendNotFoundError:  #don't know the error :(
-                print('Error while selecting backend.')
+                print('[{0}!{1}]Error while selecting backend.'.format(Fore.Red,Fore.Reset))
         else:
             backend=BasicAer.get_backend('qasm_simulator')
-        print('Backend selected: ',backend.name())
+        print('Backend selected: {0}{1} ({2}){3}'.format(Fore.LIGHTBLUE_EX,backend.name(),backend.configuration().n_qubits,Fore.RESET))
         return backend;
 
 #y=notyetdecided(False,'024257bbd2ae5845824e3650cd45561fca6cdff0284dc34870113b886144a3ded1c691a8d263d37fb3e5b282a5a5bcdf4573296e632221992acf75cfabb1f65c').setup_systems()
@@ -70,9 +81,9 @@ class notyetdecided:
 
 def runtime_mode():
     helpf='bruh, give a system name. Use \'-ls\' or \'--list-system\' to list available systems.'
-    parser=argparse.ArgumentParser(prog='timemachine',usage='use %(prog)s [runon]...',prefix_chars='-',formatter_class=RawTextHelpFormatter,epilog='VOADG')
+    parser=argparse.ArgumentParser(prog='timemachine',prefix_chars='-',formatter_class=RawTextHelpFormatter,epilog='VOADG')
     yes=parser.add_argument_group('yes')
-    yes.add_argument('-yv','--yesv',dest='backend',action='store',help=helpf)
+    yes.add_argument('-yv','--yesv',dest='backend',action='store',help=helpf) #everyone gets one :)
     yes.add_argument('-yo','--yesowen',dest='backend',action='store',help=helpf)
     yes.add_argument('-ya','--yesangy',dest='backend',action='store',help=helpf)
     yes.add_argument('-yd','--yesdavid',dest='backend',action='store',help=helpf)
@@ -84,9 +95,12 @@ def runtime_mode():
 
     args_parsed=parser.parse_args()
     if args_parsed.toke:
-        globals['token']=args_parsed.toke
+        #globals['token']=args_parsed.toke
+        config.set('auth','token',args_parsed.toke)
+        config.write(sys.stdout)
+        print('Token loaded in \'{0}config.ini{1}\''.format(Fore.LIGHTRED_EX,Fore.RESET))
     if not args_parsed.lb and not args_parsed.ls and not args_parsed.backend and not args_parsed.siml:
-        print('\nNo args parsed.')
+        print('\n[{0}!{1}]No args parsed.'.format(Fore.RED,Fore.RESET))
         sys.exit(0)
     if args_parsed.lb:
         lb=notyetdecided(simul=False,leastbusy=True,tok=token).setup_systems(needsystem=False)
@@ -97,5 +111,17 @@ def runtime_mode():
         beekend=notyetdecided(simul=False,tok=token).setup_systems(args_parsed.backend,needsystem=True)
     if args_parsed.siml:
         simm=notyetdecided(simul=True).setup_systems(needsystem=False)
-    
+
+# Q R N G
+'''
+start_time = time.time()
+job_status = job.status()
+while job_status not in JOB_FINAL_STATES:
+    print(f'Status @ {time.time()-start_time:0.0f} s: {job_status.name},'
+          f' est. queue position: {job.queue_position()}')
+    time.sleep(10)
+    job_status = job.status()'''
+
+def QRNG():
+
 runtime_mode()
