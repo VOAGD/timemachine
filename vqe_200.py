9bc5454d6239da3d4283e9ca87f0fbea37c22151
532stary4 <61381495+532stary4@users.noreply.github.com>
2021-02-18 16:10:43 +0000

    Update vqe_200.py

--- vqe_200.py
+++ vqe_200.py
@@ -24,8 +24,28 @@ def variational_ansatz(params, wires):
 
     # QHACK #
     qml.templates.state_preparations.ArbitraryStatePreparation(params,wires=wires)
-    #weight= np.random.uniform(- np.pi/2, np.pi/2, size=(63,))
-    #qml.broadcast(unitary=qml.templates.ArbitraryUnitary(weight,wires), pattern="double", wires=wires)
+    '''n_qubits = len(wires)
+    n_rotations = len(params)
+
+    if n_rotations > 1:
+        n_layers = n_rotations // n_qubits
+        n_extra_rots = n_rotations - n_layers * n_qubits
+
+        # Alternating layers of unitary rotations on every qubit followed by a
+        # ring cascade of CNOTs.
+        for layer_idx in range(n_layers):
+            layer_params = params[layer_idx * n_qubits : layer_idx * n_qubits + n_qubits, :]
+            qml.broadcast(qml.Rot, wires, pattern="single", parameters=layer_params)
+            qml.broadcast(qml.CNOT, wires, pattern="ring")
+
+        # There may be "extra" parameter sets required for which it's not necessarily
+        # to perform another full alternating cycle. Apply these to the qubits as needed.
+        extra_params = params[-n_extra_rots:, :]
+        extra_wires = wires[: n_qubits - 1 - n_extra_rots : -1]
+        qml.broadcast(qml.Rot, extra_wires, pattern="single", parameters=extra_params)
+    else:
+        # For 1-qubit case, just a single rotation to the qubit
+        qml.Rot(*params[0], wires=wires[0])'''
     # QHACK #
 
 
@@ -50,7 +70,7 @@ def run_vqe(H):
     # Initialize the quantum device
     np.random.seed(0)
     #params = np.random.uniform(low=-np.pi / 2, high=np.pi , size=size)
-    params = np.random.uniform(0.05, 0.4, size)
+    params = np.random.uniform(0.01, 0.5, size)
     # Randomly choose initial parameters (how many do you need?)
     cost_fn = qml.ExpvalCost(variational_ansatz, H, dev0, optimize=True)
     # Set up a cost function
